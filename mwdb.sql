6ca33bb5a5333770aa8aea742df6ae4ae6ac59ab
Alexander Alpatov <werty.mn2@yandex.ru>
2021-05-13 12:02:45 +0300

    Database Structure File

---
+++ mwdb.sql
@@ -0,0 +1,77 @@
+DROP TABLE ObjectInRoute, Route, ObjectTag, Tag, Review,
+    ObjectCategory, Category, Object, User;
+
+CREATE TABLE User (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(64),
+    email VARCHAR(128),
+    password_hash VARCHAR(256),
+    rights_level INT UNSIGNED,
+    subscription_level INT UNSIGNED,
+    birthday DATE
+);
+
+CREATE TABLE Object (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256),
+    longitude INT,
+    latitude INT,
+    image_url VARCHAR(256),
+    description VARCHAR(2048),
+    rating FLOAT UNSIGNED,
+    # Age restriction levels 0+, 6+, 12+, 16+, 18+
+    age_restriction_level INT
+);
+
+CREATE TABLE Category (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256)
+);
+
+CREATE TABLE ObjectCategory (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    object_id INT UNSIGNED,
+    category_id INT UNSIGNED,
+    FOREIGN KEY (object_id) REFERENCES Object(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES Category(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE Review (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_id INT UNSIGNED,
+    object_id INT UNSIGNED,
+    text VARCHAR(1024),
+    rating INT UNSIGNED,
+    FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (object_id) REFERENCES User(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Tag (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(32)
+);
+
+CREATE TABLE ObjectTag (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    object_id INT UNSIGNED,
+    tag_id INT UNSIGNED,
+    FOREIGN KEY (object_id) REFERENCES Object(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (tag_id) REFERENCES Tag(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Route (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_id INT UNSIGNED,
+    date DATETIME DEFAULT NOW(),
+    rating FLOAT UNSIGNED,
+    FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE ObjectInRoute (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    position INT UNSIGNED,
+    object_id INT UNSIGNED,
+    route_id INT UNSIGNED,
+    FOREIGN KEY (object_id) REFERENCES Object(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (route_id) REFERENCES Route(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
