7c54633780e1d7993ef57e42e54e442cc82ae882
HehexOne <werty.mn2@yandex.ru>
2021-06-08 18:04:55 +0300

    added map constructor for points with yandex static maps api

---
+++ mapconstructor.py
@@ -0,0 +1,13 @@
+class MapConstructor:
+
+    def construct_map(self, points):
+        url = "https://static-maps.yandex.ru/1.x/?"
+        params = {
+            "ll": '37.620070,55.753630',
+            "size": "650,350",
+            "l": "map",
+            "z": "10",
+            "pt": "~".join([f"{points[i][1]},{points[i][0]},pm2dgm{i+1}" for i in range(len(points))])
+        }
+        url += "&".join([f"{key}={value}"for key, value in params.items()])
+        return url
